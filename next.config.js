c6edfc9035ca6afb014ddfb32f3915be43c6b6f3
javicond3 <4b3javi@gmail.com>
2025-02-11 11:56:58 +0100

    update basePathc

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/agents",
+    basePath: "/agentes",
     reactStrictMode: false,
     webpack: (config) => {
         config.resolve.fallback = { fs: false,path:false };
