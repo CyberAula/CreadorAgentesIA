218ef82a4b18a21c3e888e479f932517d21929f3
june <belencastillocha@gmail.com>
2025-05-30 13:22:10 +0200

    chat_embed-styles + new logo

--- app/embed/[assistantId]/page.js
+++ app/embed/[assistantId]/page.js
@@ -153,10 +153,10 @@ function Embed() {
 
     return (
         <div className="h-screen w-screen md:p-4 flex flex-col bg-myBg gap-4">
-            <div className={`flex justify-between bg-myPrimary rounded-xl p-4`}>
+            <div className={`flex justify-between bg-primary-400 rounded-xl p-4`}>
                 <div className="flex items-center gap-2">
                     <Image height={25} width={25} src={urljoin(basePath, '/assistant.svg')} alt="logo" />
-                    <span className="font-semibold">{assistantName == null ? "Ayudante Top" : assistantName}</span>
+                    <span className="font-semibold text-white">{assistantName == null ? "Ayudante Top" : assistantName}</span>
                 </div>
                 <div className="d-flex align-items-center gap-2 cursor-pointer">
                     {/*<Image height={20} width={20} onClick={refreshChat} src={urljoin(basePath, '/refresh.svg')}  alt="refresh"/>*/}
@@ -169,7 +169,7 @@ function Embed() {
 
                 {chat.map((msg, index) =>
                 //IA BUBBLE
-                    <div key={index} className={`${msg.isBot ? 'bg-gray-900 text-gray-100 self-start' : 'text-gray-900 bg-gray-100 self-end border-2'} rounded-lg  px-3 py-2 max-w-5xl`}>
+                    <div key={index} className={`${msg.isBot ? 'bg-chatbot text-text self-start' : 'text-text-inverse bg-primary-400 self-end border-2'} rounded-lg  px-3 py-2 max-w-5xl`}>
                         <ReactMarkdown
                             remarkPlugins={[remarkGfm]}
                             rehypePlugins={[rehypeHighlight]}
@@ -192,19 +192,19 @@ function Embed() {
                         </ReactMarkdown>
                     </div>)}
                     {/* Puntos loading */}
-                {loading && <div className={`bg-gray-900 text-gray-100 self-start rounded-lg  px-3 py-2 max-w-sm`}>
+                {loading && <div className="loading">
                     <div className="flex h-4 items-center gap-2">
-                        <div className="bounce bounce1 rounded-full bg-slate-500 h-2 w-2" />
-                        <div className="bounce bounce2 rounded-full bg-slate-500 h-2 w-2" />
-                        <div className="bounce bounce3 rounded-full bg-slate-500 h-2 w-2" />
+                        <div className="bounce bounce1 rounded-full bg-text h-2 w-2" />
+                        <div className="bounce bounce2 rounded-full bg-text h-2 w-2" />
+                        <div className="bounce bounce3 rounded-full bg-text h-2 w-2" />
                     </div>
                 </div>}
 
             </div>
             {/* input message */}
             <div className="flex gap-2 mt-auto">
-                <input id="question" className="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="Ask a question" required value={question} onKeyDown={(e) => { e.code == "Enter" && !e.shiftKey && askAssistant(); }} onChange={(e) => setQuestion(e.target.value)} />
-                <button onClick={askAssistant} className=" bg-mySecondary hover:bg-blue-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-4 py-2.5 text-center ">
+                <input id="question" className="input" placeholder="Ask a question" required value={question} onKeyDown={(e) => { e.code == "Enter" && !e.shiftKey && askAssistant(); }} onChange={(e) => setQuestion(e.target.value)} />
+                <button onClick={askAssistant} className=" buttonprimary">
                     <Image height={20} width={20} src={urljoin(basePath, '/send.svg')} alt="send" />
                 </button>
             </div>
